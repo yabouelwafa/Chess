244dbbfb4695a93e4b2c436beed1f90e74815c30
yabouelwafa <jabouelwafa@gmail.com>
2025-03-26 17:53:31 -0400

    Created Board and pieces

---
+++ ChessEngine.py
@@ -0,0 +1,19 @@
+"""
+Storing Data and determining the valid moves and keeping a move log.
+"""
+
+class GameState:
+    def __init__(self):
+        #Board is 8x8 2-dimensional list
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--",],
+            ["--", "--", "--", "--", "--", "--", "--", "--", ],
+            ["--", "--", "--", "--", "--", "--", "--", "--", ],
+            ["--", "--", "--", "--", "--", "--", "--", "--", ],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
